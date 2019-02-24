3d6a9e82614adb9875b9ae49ecc1756884148c44
Farina Ashfaq <fari.ash16@gmail.com>
2019-02-24 15:59:58 +0500

    Update Code

---
+++ src/js/components/AddTask.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="modal-body" id="modal-body">
+    <div class="modal" id="modal">
+      <div class="right">
+        <div @click="handleAddTask" class="close-container center gray" id="close">
+          <img src="../../assets/images/cancel.png" />
+        </div>
+      </div>
+      <div>
+        <h1 class="modal-heading">Add Task</h1>
+        <p class="gray-p">Tackle your goals in daily doses</p>
+
+        <div class="input-container">
+          <p class="input-lable-p">Name the task</p>
+          <input type="text" placeholder="e.g. Study Spanish" />
+        </div>
+        <div class="input-container2">
+          <p class="input-lable-p">Daily Task</p>
+          <div class="two-cols">
+            <div class="two-cols" v-bind:style="{justifyContent: 'space-between',alignItems: 'center', gridGap: '20px'}">
+              <input type="number" placeholder="00" />
+              <p>Hours</p>
+            </div>
+            <div class="two-cols" v-bind:style="{justifyContent: 'space-between',alignItems: 'center', gridGap: '20px'}">
+              <input type="number" placeholder="00" />
+              <p>Minutes</p>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+      <div class="input-container">
+        <p class="input-lable-p">Name the task</p>
+        <div class="seven-columns">
+          <input type="text" placeholder="Mon" />
+          <input type="text" placeholder="Tue" />
+          <input type="text" placeholder="Wed" />
+          <input type="text" placeholder="Thu" />
+          <input type="text" placeholder="Fri" />
+          <input type="text" placeholder="Sat" />
+          <input type="text" placeholder="Sun" />
+        </div>
+      </div>
+      <div class="input-container">
+        <p class="input-lable-p">Task color</p>
+        <select>
+          <option class="option"><span></span> Pink</option>
+          <option class="option"><span></span> Blue</option>
+          <option class="option"><span></span> Red</option>
+        </select>
+        <div class="two-cols" v-bind:style="{paddingTop: '20px'}">
+          <button class="save-btn">Save Task</button>
+          <div class="center">
+            <p class="addTask"><span> + </span> Add Another</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'AddTask',
+    methods: {
+      handleAddTask(){
+        this.$emit('handleAddTask', false)
+      },
+    }
+  };
+</script>
--- src/js/components/TodoApp.vue
+++ src/js/components/TodoApp.vue
@@ -1,67 +1,6 @@
 <template>
   <div>
-    <section v-if="modalOpen" class="modal-body" id="modal-body">
-      <div class="modal" id="modal">
-        <div class="right">
-          <div @click="handleAddTask(false)" class="close-container center gray" id="close">
-            <img src="../../assets/images/cancel.png" />
-          </div>
-        </div>
-        <div>
-          <h1 class="modal-heading">Add Task</h1>
-          <p class="gray-p">Tackle your goals in daily doses</p>
-
-          <div class="input-container">
-            <p class="input-lable-p">Name the task</p>
-            <input type="text" placeholder="e.g. Study Spanish" />
-          </div>
-          <div class="input-container2">
-            <p class="input-lable-p">Daily Task</p>
-            <div class="two-cols">
-              <div class="two-cols" v-bind:style="{justifyContent: 'space-between',alignItems: 'center', gridGap: '20px'}">
-                <input type="number" placeholder="00" />
-                <p>Hours</p>
-              </div>
-              <div class="two-cols" v-bind:style="{justifyContent: 'space-between',alignItems: 'center', gridGap: '20px'}">
-                <input type="number" placeholder="00" />
-                <p>Minutes</p>
-              </div>
-
-            </div>
-
-          </div>
-
-        </div>
-        <div class="input-container">
-          <p class="input-lable-p">Name the task</p>
-          <div class="seven-columns">
-            <input type="text" placeholder="Mon" />
-            <input type="text" placeholder="Tue" />
-            <input type="text" placeholder="Wed" />
-            <input type="text" placeholder="Thu" />
-            <input type="text" placeholder="Fri" />
-            <input type="text" placeholder="Sat" />
-            <input type="text" placeholder="Sun" />
-          </div>
-        </div>
-        <div class="input-container">
-          <p class="input-lable-p">Task color</p>
-          <select>
-            <option class="option"><span></span> Pink</option>
-            <option class="option"><span></span> Blue</option>
-            <option class="option"><span></span> Red</option>
-          </select>
-          <div class="two-cols" v-bind:style="{paddingTop: '20px'}">
-            <button class="save-btn">Save Task</button>
-            <div class="center">
-              <p class="addTask"><span> + </span> Add Another</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-
+    <add-task v-if="modalOpen" @handleAddTask="handleAddTask"/>
     <section class="main-container">
       <div class="left-container">
         <header>
@@ -280,6 +219,7 @@
 </template>
 
 <script>
+  import AddTask from './AddTask';
 export default {
   name: 'hello',
   data() {
@@ -291,6 +231,9 @@ export default {
       setopacity_block: '1',
       setopacity_left: '0.25',
     };
+  },
+  components:{
+    'add-task': AddTask
   },
   created(){
 
